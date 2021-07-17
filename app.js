d8a1f79672af8361c14ccd9bee2079584d06c88d
elephantcastle <obymik89@gmail.com>
2021-07-17 17:37:44 +0100

    app.js setup; endpoints for agents, logs, resolution

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+let agents = JSON.parse(fs.readFileSync("./json-data/agents.json"));
+let logs = JSON.parse(fs.readFileSync("./json-data/logs.json"));
+let resolution = JSON.parse(fs.readFileSync("./json-data/resolution.json"));
+
+const app = express();
+app.use(cors());
+
+app.get("/agents", function (req, res, next) {
+  res.json(agents);
+});
+
+app.get("/logs", function (req, res, next) {
+  res.json(logs);
+});
+
+app.get("/resolution", function (req, res, next) {
+  res.json(resolution);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
